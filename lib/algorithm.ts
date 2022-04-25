cff31926c443111dced82e8a7d30715933b5f266
Cory Etzkorn <cory@makenotion.com>
2022-04-25 19:43:11 -0400

    More rules

--- lib/algorithm.ts
+++ lib/algorithm.ts
@@ -4,11 +4,14 @@
 // ---------------------------
 
 export function rank(tweet: string) {
-  const parsed = tweet.toLowerCase()
-  const sum = [pepe(parsed), elon(parsed)].reduce(
-    (partialSum, a) => partialSum + a,
-    0
-  )
+  const parsedTweet = tweet.toLowerCase()
+  const rules = [
+    pepe(parsedTweet),
+    elon(parsedTweet),
+    conservatives(parsedTweet),
+    liberals(parsedTweet),
+  ]
+  const sum = rules.reduce((partialSum, a) => partialSum + a, 0)
   if (sum < 0) {
     // 0 is the minimum score
     return 0
@@ -27,16 +30,64 @@ export function rank(tweet: string) {
 // Add new rules here!
 // ---------------------------
 
-function pepe(tweet: string) {
-  if (tweet.indexOf("🐸") >= 0) {
-    return 50
+/**
+ * Suppress conservative voices
+ */
+function conservatives(tweet: string) {
+  const people = [
+    "donald trump",
+    "trump",
+    "rudy giuliani",
+    "rush limbaugh",
+    "dick cheney",
+    "laura ingraham",
+  ]
+  const matches = people.map((person) => {
+    const regex = new RegExp(`\\b${person}\\b`, "gi")
+    return (tweet.match(regex) || []).length
+  })
+  const totalMatches = matches.reduce((partialSum, a) => partialSum + a, 0)
+  const scorePerMatch = -5
+  return totalMatches * scorePerMatch
+}
+
+/**
+ * Amplify liberal voices
+ */
+function liberals(tweet: string) {
+  const people = [
+    "clinton",
+    "hilary clinton",
+    "obama",
+    "barack obama",
+    "biden",
+    "joe biden",
+  ]
+  const matches = people.map((person) => {
+    const regex = new RegExp(`\\b${person}\\b`, "gi")
+    return (tweet.match(regex) || []).length
+  })
+  const totalMatches = matches.reduce((partialSum, a) => partialSum + a, 0)
+  const scorePerMatch = 10
+  return totalMatches * scorePerMatch
+}
+
+/**
+ * Make sure Elon is always viral
+ */
+function elon(tweet: string) {
+  if (tweet.indexOf("elon") >= 0) {
+    return 100
   }
   return 0
 }
 
-function elon(tweet: string) {
-  if (tweet.indexOf("elon") >= 0) {
-    return 25
+/**
+ * This frog emoji seems cute
+ */
+function pepe(tweet: string) {
+  if (tweet.indexOf("🐸") >= 0) {
+    return 50
   }
   return 0
 }
